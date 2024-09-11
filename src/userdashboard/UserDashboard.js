ee8be3fcf511e291856ad39cd21edd24eb1738a9
vaibhavmakwana45 <vaibhav_makwana@sparrowsofttech.com>
2024-09-11 22:44:53 +0530

    push request

--- src/userdashboard/UserDashboard.js
+++ src/userdashboard/UserDashboard.js
@@ -10,6 +10,7 @@ import {
 import React, { useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 import AxiosInstance from "config/AxiosInstance";
+import "./style.css";
 
 export default function NavratriPass() {
   const cardBgImage = "/path/to/your/background-image"; // Add your custom background image here
@@ -66,122 +67,54 @@ export default function NavratriPass() {
     : cardBgImage;
 
   return (
-    <Flex justifyContent="center" alignItems="center">
-      <Box
-        w="450px"
-        h="700px"
-        borderRadius="20px"
-        boxShadow="2xl"
-        textAlign="center"
-        p={6}
-        bgImage={bgImage}
-        bgSize="100% 100%"
-        bgPosition="center"
-        color={textColor}
-        // backdropFilter="blur(120px)"
-        border="3px solid #FF7F50"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        position="relative"
-      >
-        <Box
-          position="absolute"
-          top="0"
-          left="0"
-          right="0"
-          bottom="0"
-          background="rgba(0, 0, 0, 0.4)"
-          borderRadius="20px"
-          zIndex="-1"
+    <section
+      className="home"
+      style={{
+        backgroundImage: `${bgImage}`,
+      }}
+    >
+      <div className="scalloped-shape">
+        <img
+          src={`https://cdn.savajcapital.com/cdn/files/${userData?.userimage}`}
+          alt="User"
         />
-
-       
-          {/* <Box
-            borderRadius="full"
-            w="250px" // Increased width
-            h="250px" // Increased height
-            overflow="hidden"
-            mx="auto"
-            boxShadow="lg"
-            border="4px solid #FFAD60"
-            position="relative"
-            zIndex="1"
-            bg="white"
-          >
-            <Image
-              src={`https://cdn.savajcapital.com/cdn/files/${userData?.userimage}`}
-              alt="User"
-              objectFit="cover"
-              boxSize="100%"
-              borderRadius="full"
-            />
-          </Box> */}
-
-          {/* 
-          <Heading
-            fontSize="4xl"
-            color="yellow.300"
-            textShadow="3px 3px 4px #FF6F61"
-            fontWeight="extrabold"
-            mb={4}
-            textTransform="uppercase"
-          >
-            Navratri Festival Pass
-          </Heading> */}
-          {/* <Text
-            fontSize="2xl"
-            color="yellow.300"
-            fontWeight="bold"
-            mb={2}
-            textShadow="2px 2px 4px #FF6F61"
-          >
-            2024
-          </Text> */}
-          {/* <Text
-            fontSize="md"
-            px={6}
-            color="white"
-            textShadow="10px 1px 4px #FF6F61"
-            mb={6}
-          >
-            Celebrate the spirit of Navratri with vibrant dance, music, and joy!
-          </Text> */}
-          {/* 
-          <Box
-            width="100%"
-            bg="rgba(255, 255, 255, 0.8)"
-            borderRadius="md"
-            boxShadow="md"
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            p={4}
-            backdropFilter="blur(4px)"
-            textAlign="center"
-            border="2px dashed #D9534F"
-            mb={4}
-          >
-            <Text fontSize="sm" fontWeight="bold" color="gray.700">
-              Name: {userData?.fullname}
-            </Text>
-            <Text fontSize="sm" fontWeight="bold" color="gray.700">
-              Email: {userData?.email}
-            </Text>
-          </Box> */}
-
-          {/* <Image
-            src={`https://cdn.savajcapital.com/cdn/files/${userData?.groupimage}`}
-            alt="Group"
-            boxSize="150px"
-            objectFit="cover"
-            borderRadius="md"
-            boxShadow="lg"
-          /> */}
-        
-      </Box>
-    </Flex>
+        <div className="name-text">
+          <h1>
+            Name: <span>Darshan Vaghani</span>
+          </h1>
+          <h1>
+            Mo No: <span>+91991391499</span>
+          </h1>
+        </div>
+      </div>
+    </section>
   );
 }
+
+// import React from "react";
+// import "./style.css";
+
+// const NavratriPass = () => {
+//   return (
+//     <section
+//       className="home"
+//       style={{
+//         backgroundImage: "url(./day1.png)",
+//       }}
+//     >
+//       <div className="scalloped-shape">
+//         <img src="./user-2.jpg" alt="User" />
+//         <div className="name-text">
+//           <h1>
+//             Name: <span>Darshan Vaghani</span>
+//           </h1>
+//           <h1>
+//             Mo No: <span>+91991391499</span>
+//           </h1>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default NavratriPass;
